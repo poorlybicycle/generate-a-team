30c058b99fcc7baf86cf267ccb8a7ca291dd5235
Mime Jr <petravidovic8@gmail.com>
2022-10-26 20:55:45 -0400

    add inquirer and some question prompts

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const inquirer = require('inquirer');
 const generateHTML = require('./generateHTML');
 var fs = require('fs');
 var answerArray = [];
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                when: () => answerArray.length == 0,
+                type: 'input',
+                name: 'managerName',
+                message: 'What is the name of your team manager?',
+            },
+            {
+                when: () => answerArray.length == 0,
+                type: 'input',
+                name: 'managerEmployeeID',
+                message: "What is the team manager's employee ID?",
+            },
+            {
+                when: () => answerArray.length > 0,
+                type: 'list',
+                name: 'addEmployee',
+                message: 'Want to add one of the following employees?:',
+                choices: ['Intern', 'Engineer', 'Manager'],
+            },
+        ])
+}
